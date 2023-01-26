821f536527e4b2b9b8f317e21b2e1d3a8ff700b8
nyacoEngineer <nyacoengineer@gmail.com>
2023-01-26 12:50:09 +0900

    image_pickerのWeb利用

--- lib/main.dart
+++ lib/main.dart
@@ -23,18 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -42,7 +31,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   XFile? _image;
   final imagePicker = ImagePicker();
-
   // カメラから画像を取得するメソッド
   Future getImageFromCamera() async {
     final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
@@ -56,10 +44,12 @@ class _MyHomePageState extends State<MyHomePage> {
   // ギャラリーから画像を取得するメソッド
   Future getImageFromGarally() async {
     final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+    print(pickedFile);
     // Image.network(pickedFile!.path);
     setState(() {
       if (pickedFile != null) {
         _image = XFile(pickedFile.path);
+        print(_image);
       }
     });
   }
@@ -77,11 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     '写真を選択してください',
                     style: Theme.of(context).textTheme.headline4,
                   )
-                : Image.network(
-                    'https://pbs.twimg.com/profile_images/1318213516935917568/mbU5hOLy_400x400.png',
-                    width: 128,
-                    height: 128,
-                  )),
+                : Image.network(_image!.path)),
         floatingActionButton:
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           // カメラから取得するボタン
